d37e27f276d4c8940662fe77d21fd38c7dad0d7c
mikan19 <mellamo.rina.ty@gmail.com>
2023-04-23 18:18:41 +0900

    fix: $numの出力内容の修正

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,10 @@
 <?php
-echo 'Welcome TECH QUEST!';
+$num = 72;
+
+if ($num % 12 == 0 && $num % 24 == 0) {
+    echo "numの値は12の倍数かつ、24の倍数です";
+} elseif ($num % 24 == 0) {
+    echo "numの値は24の倍数です";
+} elseif($num % 12 == 0) {
+    echo "numの値は12の倍数です";
+} 
